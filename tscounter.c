3e2bd554ae6bd8a73da860bd1b8971126742e102
Somyung Yun <dbsthaud99@gmail.com>
2021-06-13 19:57:15 +0900

    change mutex to semaphore

--- tscounter.c
+++ tscounter.c
@@ -3,10 +3,26 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+union semun {
+    int val;
+    struct semid_ds *buf;
+    ushort *array;
+};
+
+#define PATH "/home/yunsomyung/key"
 
 typedef struct __counter_t {
     int value;
-    pthread_mutex_t lock;
+
+    key_t key;
+    int semid;
+
+    union semun arg;
+    
 } counter_t;
 
 unsigned int loop_cnt;
@@ -14,25 +30,63 @@ counter_t counter;
 
 void init(counter_t *c) {
     c->value = 0;
-    pthread_mutex_init(&c->lock, NULL);
+    
+    c->key = ftok(PATH, 'z');
+    c->semid = semget(c->key, 1, 0600 | IPC_CREAT);
+    
+    printf("semid = %d\n", c->semid);
+
+    c->arg.val = 1;
+    semctl(c->semid, 0, SETVAL, c->arg);
 }
 
 void increment(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
+    struct sembuf s;
+
+    s.sem_num = 0;
+    s.sem_op = -1;
+    s.sem_flg = 0;
+    semop(c->semid, &s, 1);
+
     c->value++;
-    pthread_mutex_unlock(&c->lock);
+    
+    s.sem_num = 0;
+    s.sem_op = 1;
+    s.sem_flg = 0;
+    semop(c->semid, &s, 1);
 }
 
 void decrement(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
+    struct sembuf s;
+
+    s.sem_num = 0;
+    s.sem_op = -1;
+    s.sem_flg = 0;
+    semop(c->semid, &s, 1);
+
     c->value--;
-    pthread_mutex_unlock(&c->lock);
+    
+    s.sem_num = 0;
+    s.sem_op = 1;
+    s.sem_flg = 0;
+    semop(c->semid, &s, 1);
 }
 
 int get(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
+    struct sembuf s;
+
+    s.sem_num = 0;
+    s.sem_op = -1;
+    s.sem_flg = 0;
+    semop(c->semid, &s, 1);
+    
     int rc = c->value;
-    pthread_mutex_unlock(&c->lock);
+    
+    s.sem_num = 0;
+    s.sem_op = 1;
+    s.sem_flg = 0;
+    semop(c->semid, &s, 1);
+    
     return rc;
 }
 
@@ -54,6 +108,16 @@ int main(int argc, char *argv[])
     loop_cnt = atoi(argv[1]);
 
     init(&counter);
+
+    if (counter.key < 0) {
+        perror(argv[0]);
+        exit(1);
+    }
+
+    if (counter.semid < 0) {
+        perror(argv[0]);
+        exit(1);
+    }
 
     pthread_t p1, p2;
     printf("main: begin [counter = %d]\n", get(&counter));
